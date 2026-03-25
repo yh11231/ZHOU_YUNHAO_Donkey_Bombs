778716b93fa777543ccef2979f69d233c01bdb17
yh11231 <yunhaozhou529@gmail.com>
2026-03-25 10:00:04 +0100

    Update agent7.py
    renomer le nom du agent pour exo2

--- src/agents/team7/agent7.py
+++ src/agents/team7/agent7.py
@@ -11,7 +11,7 @@ class Agent7(KartAgent):
         self.agent_positions = []
         self.obs = None
         self.isEnd = False
-        self.name = "Team7" # replace with your chosen name
+        self.name = "ZHOU_YUNHAO" # replace with your chosen name
 
     def reset(self):
         self.obs, _ = self.env.reset()
