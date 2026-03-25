705fc5c7d4f51235ee9d147b9111b5ab98b875a9
yh11231 <yunhaozhou529@gmail.com>
2026-03-25 09:19:28 +0100

    Update agent6.py
    Renomer

--- src/agents/team6/agent6.py
+++ src/agents/team6/agent6.py
@@ -12,7 +12,7 @@ class Agent6(KartAgent):
         self.agent_positions = []
         self.obs = None
         self.isEnd = False
-        self.name = "Team6" # replace with your chosen name
+        self.name = "ZHOU_YUNHAO" # replace with your chosen name
 
     def reset(self):
         self.obs, _ = self.env.reset()
