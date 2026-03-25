990836399561e1d3799b093ce724ed1776cdcba3
yh11231 <yunhaozhou529@gmail.com>
2026-03-25 09:45:10 +0100

    Update agent6.py
    exo1

--- src/agents/team6/agent6.py
+++ src/agents/team6/agent6.py
@@ -4,6 +4,9 @@ import random
 from utils.track_utils import compute_curvature, compute_slope
 from agents.kart_agent import KartAgent
 
+#from .Mid import Mid 
+
+
 
 class Agent6(KartAgent):
     def __init__(self, env, path_lookahead=3):
@@ -14,6 +17,8 @@ class Agent6(KartAgent):
         self.isEnd = False
         self.name = "ZHOU_YUNHAO" # replace with your chosen name
 
+
+        self.dernier_distance_centre = 0.0 #initialiser le dernier distance du centre a 0
     def reset(self):
         self.obs, _ = self.env.reset()
         self.agent_positions = []
@@ -22,15 +27,27 @@ class Agent6(KartAgent):
         return self.isEnd
 
     def choose_action(self, obs):
-        acceleration = random.random()
-        steering = random.random()
+        distance_centre = obs['center_path_distance'][0]  # distance au centre 
+
+        # si la distance est plus loin du centre, on tourne plus fort, avec un valeur de controle pour tourner doux
+        erreur = 0.35 * distance_centre
+        # la différence avec la dernière distance centre pour éviter les virages brusques
+        diff = 0.77 * (distance_centre - self.dernier_distance_centre)
+        # angle de tournage, lopposé avec la distance au centre, si la distance est positive (droite), on tourne à gauche etc.
+        steer = -(erreur + diff)
+        # min et max pour limiter les tourne trop forts
+        steer = max(-0.6, min(0.6, steer))
+        
+        # 3. mettre en jour le dernier distance centre pour le prochaine calcule
+        self.dernier_distance_centre = distance_centre
+        brake = 0.0
         action = {
-            "acceleration": acceleration,
-            "steer": steering,
-            "brake": False, # bool(random.getrandbits(1)),
-            "drift": bool(random.getrandbits(1)),
-            "nitro": bool(random.getrandbits(1)),
-            "rescue":bool(random.getrandbits(1)),
+            "acceleration": 1,
+            "steer": steer,
+            "brake": brake,
+            "drift": False,  
+            "nitro": False,
+            "rescue": bool(random.getrandbits(1)),
             "fire": bool(random.getrandbits(1)),
         }
         return action
